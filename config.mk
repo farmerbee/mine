851454b7672c94fec46bdc6b6d01c97c8c522f6e
farmer <896289562@qq.com>
2022-05-31 16:31:08 +0800

    replace makefile with cmakelists

--- config.mk
+++
@@ -1,8 +0,0 @@
-export BUILD_ROOT = ${shell pwd}
-
-export INCLUDE_DIR = ${BUILD_ROOT}/include 
-
-export BIN_DIR = ${BUILD_ROOT}/bin
-
-export BUILD_DIR = ${BUILD_ROOT}/helper \
-					${BUILD_ROOT}/test 
